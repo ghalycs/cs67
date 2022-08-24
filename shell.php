03481708c3d64709d650e030b89334a4a912830f
ghalycs <104019188+ghalycs@users.noreply.github.com>
2022-08-24 18:49:01 +0200

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,6 +1,6 @@
 <?php
 
-echo("<font color=red><h1>generation</h1></font>\n<font color=white><h2>feed</h2></font>\n<font color=black><h3>using</h3></font>\n");
+echo("<font color=red><h1>generation</h1></font>\n<font color=blue><h2>feed</h2></font>\n<font color=black><h3>using</h3></font>\n");
 
 
 ?>
